3505ab58abf453832919fb3ece776937c6753205
Toshi <toshiaki.ikeda.study@gmail.com>
2017-09-24 20:48:59 +0900

    “admin_and_error”

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   resources :studysheets, only: [:index, :new, :create, :show, :edit, :update, :destroy]do
     collection do
